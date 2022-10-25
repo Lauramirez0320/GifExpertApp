37a080fa8a445d2a986b9a3ce3e25ab56a143422
Laura Ramirez <lauramirez0320@gmail.com>
2022-10-24 20:42:35 -0500

    Proyect created

---
+++ src/GifExpertApp.tsx
@@ -0,0 +1,11 @@
+//pK8UwLNHBhn85q3RdONULuAVbUJcejnM
+
+const GifExpertApp = () => {
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+        </>
+    );
+};
+
+export default GifExpertApp;
